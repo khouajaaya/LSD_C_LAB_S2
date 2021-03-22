f0e7472388d250eb9038f95787ac8d5a1b7033a8
khouajaaya <80949528+khouajaaya@users.noreply.github.com>
2021-03-22 11:26:45 +0100

    TP1 EX3

--- TP1/EX3/primes.c
+++ TP1/EX3/primes.c
@@ -8,12 +8,30 @@
 
 static bool    is_prime(int n)
 {
-    // write is_prime function code here
+    if (n==2)
+    {
+        return 1;
+    }
+
+    for (int i = 2; i < n; i++)
+    {
+        if(n%i==0){
+            return 0;
+        }
+    }
+    return 1;
 }
 
 
 void    get_primes(int n)
+    
 {
-    // write get_primes function code here that use is_prime function
+ for (int i = 2; i < n; i++)
+    {
+        if(is_prime(i)==1){
+            printf("%d\n",i);
+        }
+    }
+    return;
 }
 
