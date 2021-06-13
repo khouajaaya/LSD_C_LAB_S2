ce018a93f33460845fc38f32cdcafa1660b00886
khouajaaya <80949528+khouajaaya@users.noreply.github.com>
2021-06-13 12:55:19 +0100

    Update main.c

--- TP1/EX1/main.c
+++ TP1/EX1/main.c
@@ -1,26 +1,8 @@
-#include "header.h"
+SRC = main.c iterative_fibo.c recursive_fibo.c
+PRO_NAME= prog
 
-int main()
-{
-    for (int i = 0; i < 40; i++)
-    {
-        int res;
-        clock_t begin, end;
+all:
+	gcc $(SRC) -o $(PRO_NAME)
 
-        /* Calling recursive fibonacci function */
-        begin = clock(); // get the time of begining
-        res = recursive_fibo(i);
-        end = clock(); // get the time of ending
-        printf("recursive_fibo(%d) = %d \tTime elpased is %f\n",
-            i,res, (double)(end - begin) / CLOCKS_PER_SEC);
-
-        /* Calling iterative fibonacci function */
-        begin = clock(); // get the time of begining
-        res = iterative_fibo(i);
-        end = clock(); // get the time of ending
-        printf("iterative_fibo(%d) = %d \tTime elpased is %f\n",
-            i, res, (double)(end - begin) / CLOCKS_PER_SEC);
-        printf("==========================================================\n");
-    }
-    return 0;
-}
+clean:
+	rm $(PRO_NAME)
