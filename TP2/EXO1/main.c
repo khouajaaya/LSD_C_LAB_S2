d215bbed0168589ad6b39dcf2c0bc6b33320e024
khouajaaya <80949528+khouajaaya@users.noreply.github.com>
2021-06-13 13:24:27 +0100

    Create main.c

---
+++ TP2/EXO1/main.c
@@ -0,0 +1,12 @@
+#include"header.h"
+int main()
+	{
+		t_point *dim = allocateStruct();
+
+	//printf("enter two integers");
+	//scanf("%d%d", &(dim.a), &(dim.b));
+	printf("%d%d",dim->a,dim->b);
+	allocation(t_point dim);
+
+
+return 0;
